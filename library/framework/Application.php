7eef5ca4234a05575796e794b30b7d3d12e08d3c
Dommsday <Julienzimmermann@msn.com>
2018-10-05 15:13:35 +0200

    Ecriture de la méthode 'getController' dans Application.php

--- library/framework/Application.php
+++ library/framework/Application.php
@@ -14,6 +14,44 @@ abstract class Application{
 		$this->name = '';
 	}
 
+	//Récupère le bon controlleur
+	public function getController(){
+
+		$routeur  = new Routeur;
+
+		$xml = new \DOMDocument;
+		$xml->load(__DIR__.'/../../'.$this->name.'/Routeur/routes.xml');
+
+		foreach ($routes as $route){
+
+			$arrayVars = [];
+
+			//Si une variable est présente dans l'URL
+			if($route->attributeExist('vars')){
+				$vars = explode(',', $route->getAttribute('vars'));
+			}
+
+			$router->addRoute(new Route($route->getAttribute('url'), $route->getAttribute('module'), $route->getAttribute('action'), $vars));
+		}
+
+		try{
+			$matchedRoute = $router->getRoute($this->httpRequest->requestURI());
+		}catch(\RuntimeException $e){
+
+			if($e->getCode == Routeur::ERR_ROUTE){
+
+				$this->httpResponse->page404();
+			}
+		}
+
+		$_GET = array_merge($_GET, $matchedRoute->varName());
+
+		$controllerClass = $this->name.'\\Modules\\'.$matchedRoute->module().'\\'.$matchedRoute->module.'Controller';
+
+		return new $controllerClass($this, $matchedRoute->module(), $matchedRoute->action());
+
+
+	}
 	//Méthode qui doit être réécrite chez l'enfant
 	abstract public function run();
 
