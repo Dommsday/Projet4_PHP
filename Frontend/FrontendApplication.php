a195f6003a8c0adc0d558b1522060bf477ca6391
Dommsday <Julienzimmermann@msn.com>
2018-10-08 14:06:26 +0200

    Ajout de l'utilisation d'espasce de nom

--- Frontend/FrontendApplication.php
+++ Frontend/FrontendApplication.php
@@ -2,11 +2,15 @@
 
 namespace Frontend;
 
+use framework/Application:
+
 class FrontendApplication extends Application{
 
 	public function __construct(){
 
 		parent::__construct();
+
+		$this->name = 'Frontend';
 	}
 
 	public function run(){
