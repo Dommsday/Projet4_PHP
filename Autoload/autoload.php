bdbd135b3be1bcc14d7061bfae53096611bd4c5e
Dommsday <Julienzimmermann@msn.com>
2018-10-08 14:29:32 +0200

    Création et écriture du fichier permettant de charger le classe -> Autoload/autoload.php*

---
+++ Autoload/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+const DEFAULT_APP = 'Frontend';
+
+if(!isset($_GET['app']) || !file_exists(_DIR__.'/../Frontend')) $_GET['app'] = DEFAULT_APP;
+
+require __DIR__.'/../library/framework/ClassLoader.php';
+
+$libraryLoader = new ClassLoader('framework', __DIR__.'/../library');
+$libraryLoader->register();
+
+$FrontLoader = new ClassLoader('Frontend', __DIR.'/..');
+$FrontLoader->register();
