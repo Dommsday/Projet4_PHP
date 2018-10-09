2f1071dd060760abdf6f0ed6cc2082c288d9d295
Dommsday <Julienzimmermann@msn.com>
2018-10-09 19:02:40 +0200

    Ajout de deux autoloader

--- Autoload/autoload.php
+++ Autoload/autoload.php
@@ -2,15 +2,30 @@
 
 const DEFAULT_APP = 'Frontend';
 
-if(!isset($_GET['app']) || !file_exists(_DIR__.'/../Frontend')) $_GET['app'] = DEFAULT_APP;
+echo $_SERVER['REQUEST_URI'];
+
+
+if(!isset($_GET['app']) || !file_exists(__DIR__.'/../Frontend')){
+	
+	$_GET['app'] = DEFAULT_APP;
+} 
 
 require __DIR__.'/../library/framework/ClassLoader.php';
 
-$libraryLoader = new ClassLoader('framework', __DIR__.'/../library');
+$libraryLoader = new SplClassLoader('framework', __DIR__.'/../library');
 $libraryLoader->register();
 
-$FrontLoader = new ClassLoader('Frontend', __DIR__.'/..');
+$FrontLoader = new SplClassLoader('Frontend', __DIR__.'/..');
 $FrontLoader->register();
+
+$modelLoader = new SplClassLoader('Model', __DIR__.'/../library/vendors');
+$modelLoader->register();
+
+$entityLoader = new SplClassLoader('Entity', __DIR__.'/../library/vendors');
+$entityLoader->register();
 
 //Instancie la classe Frontend/FrontendApplication
 $appClass = 'Frontend'.'\\'.$_GET['app'].'Application';
+
+$app = new $appClass;
+$app->run();
