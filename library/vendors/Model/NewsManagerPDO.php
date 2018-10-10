9104a4a194151ee63236099d77fdb766ba3ef8b5
Dommsday <Julienzimmermann@msn.com>
2018-10-10 09:59:16 +0200

    Modification de quelques erreurs de frappes

--- library/vendors/Model/NewsManagerPDO.php
+++ library/vendors/Model/NewsManagerPDO.php
@@ -2,27 +2,31 @@
 
 namespace Model;
 
-use Entity\News;
+use \Entity\News;
 
 class NewsManagerPDO extends NewsManager{
 
-	public function getList($debut = -1, $limitr = -1){
+	public function getList($debut = -1, $limite = -1){
 
-		$req = 'SELECT id, id_author, title, content, date, updatedate FROM news ORDER BY id DESC';
+		$req = 'SELECT id, id_author, title, content, date, update_date FROM news ORDER BY id DESC';
 
 		if($debut != -1 || $limite != -1){
 
 			$req .= ' LIMIT '.(int) $limite.' OFFSET '.(int) $debut;
 		}
 
-		$request = $this->dao->query->($req);
-		$request->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, 'Entity\News');
+		$request = $this->dao->query($req);
+		$request->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\News');
 
-		$listeNews = $request->fetchAll();
+		$listNews = $request->fetchAll();
 
-		foreach ($listeNews as $vnews){
+		foreach ($listNews as $news){
 			$news->setDateAjout(new \DateTime($news->dateAjout()));
 			$news->setUpdateDate(new \DateTime($news->updateDate()));
 		}
+
+		$request->closeCursor();
+
+		return $listNews;
 	}
 }
