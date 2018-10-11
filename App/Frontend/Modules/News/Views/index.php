6a92ca3c184ba12e506878458e59bcce470bdc2c
Dommsday <Julienzimmermann@msn.com>
2018-10-11 14:53:22 +0200

    Modification d'une erreur de frappe

--- App/Frontend/Modules/News/Views/index.php
+++ App/Frontend/Modules/News/Views/index.php
@@ -3,8 +3,8 @@
 foreach ($listNews as $news){
 ?>
 
-<h2><a href="news-<?= $news['id']; ?>.html"><? $news['title']; ?></a></h2>
-<p><?= $news['content']; ?></p>
+<h2><a href="news-<?= $news['id'] ?>.html"><?= $news['title'] ?></a></h2>
+<p><?= $news['content'] ?></p>
 <?php
 }
 ?>
