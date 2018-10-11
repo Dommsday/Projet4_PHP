053bc5c2c795bf641023fe6ebd800b5148869ac5
Dommsday <Julienzimmermann@msn.com>
2018-10-11 11:05:35 +0200

    Modification dans la boucle foreach

--- Frontend/Modules/News/NewsController.php
+++ Frontend/Modules/News/NewsController.php
@@ -1,34 +1,38 @@
-<?php 
+<?php
+namespace App\Frontend\Modules\News;
 
-namespace Frontend\Modules\News;
+use \framework\BackController;
+use \framework\HTTPRequest;
 
-use framework\BackController;
-use framework\HTTPRequest;
-
-class NewsController extends BackController{
-
-	public function executeIndex(HTTPRequest $request){
-
-		$nombreNews = $this->app->config()->get('nombre_news');
-		$nombreCaracteres = $this->app->config()->get('nombre_caracteres');
-
-		$this->page->addVarPage('title', 'Liste des '.$nombreNews.' dernières news');
-
-		$manager = $this->managers->getManagerOf('News');
-
-		$listNews = $manager->getList(0, $nombreNews);
-
-		foreach ($listNews as $news){
-
-			if(strlen($news->contenu()) > $nombreCaracteres){
-
-				$debut = substr($news->contenu(), 0, $nombreCaracteres);
-				$debut = strlen($debut, 0, strrpos($debut, ' '). '...');
-
-				$news->setContenu($debut);
-			}
-		}
-
-		$this->page->addVarPage('listNews', $listNews);
-	}
+class NewsController extends BackController
+{
+  public function executeIndex(HTTPRequest $request)
+  {
+    $nombreNews = $this->app->config()->get('nombre_news');
+    $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+    
+    // On ajoute une définition pour le titre.
+    $this->page->addVarPage('title', 'Liste des '.$nombreNews.' dernières news');
+    
+    // On récupère le manager des news.
+    $manager = $this->managers->getManagerOf('News');
+    
+    $listNews = $manager->getList(0, $nombreNews);
+    
+    foreach ($listNews as $news)
+    {
+      if (strlen($news->content()) > $nombreCaracteres)
+      {
+        $debut = substr($news->content(), 0, $nombreCaracteres);
+        $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+        
+        $news->setContent($debut);
+      }
+    }
+    
+    // On ajoute la variable $listeNews à la vue.
+    $this->page->addVarPage('listNews', $listNews);
+  }
+  
+  
 }
