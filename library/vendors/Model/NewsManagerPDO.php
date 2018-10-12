241d2c8b03b06b961fd079806428d33bca2209fd
Juju <Julienzimmermann@msn.com>
2018-10-12 18:38:10 +0200

    Ecriture de la méthode getPost

--- library/vendors/Model/NewsManagerPDO.php
+++ library/vendors/Model/NewsManagerPDO.php
@@ -27,5 +27,23 @@ class NewsManagerPDO extends NewsManager{
 
 		return $listNews;
 	}
+    
+    public function getPost($id){
+
+		$request = $this->dao->prepare('SELECT id, author, title, content, date, update_date FROM news WHERE id = :id');
+		$request->bindValue(':id', (int) $id, \PDO::PARAM_INT);
+		$request->execute();
+
+		$request->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Entity\News');
+
+		if($news = $request->fetch()){
+
+			$news->setContent($news->content());
+
+			return $news;
+		}
+
+		return null;
+	}
 }
 
