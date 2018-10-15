8b791d7fa630e5087cbf2e638a45addd2bb291ad
Dommsday <Julienzimmermann@msn.com>
2018-10-15 12:05:39 +0200

    Modification des variables

--- App/Frontend/Modules/News/Views/post.php
+++ App/Frontend/Modules/News/Views/post.php
@@ -1,7 +1,3 @@
-<p>Par <em><?= $news['author'] ?></em>, le <?= $news['date']->format('d/m/Y à H\hi') ?></p>
-<h2><?= $news['title'] ?></h2>
-<p><?= nl2br($news['content']) ?></p>
-
-<?php if ($news['date'] != $news['update_date']) { ?>
-  <p style="text-align: right;"><small><em>Modifiée le <?= $news['update_date']->format('d/m/Y à H\hi') ?></em></small></p>
-<?php } ?>
+<p>Par <em><?= $post['author'] ?></em>, le <?= $post['date']->format('d/m/Y à H\hi') ?></p>
+<h2><?= $post['title'] ?></h2>
+<p><?= nl2br($post['content']) ?></p>
