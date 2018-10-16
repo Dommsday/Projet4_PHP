206c65b1c9e7c4cd93cd80791b49af53dcf7e86e
Dommsday <Julienzimmermann@msn.com>
2018-10-16 15:02:53 +0200

    Ajout du css

--- App/Frontend/Modules/News/Views/insertComment.php
+++ App/Frontend/Modules/News/Views/insertComment.php
@@ -1,17 +1,20 @@
-<h2>Ajouter un commentaire</h2>
+<div class="title col-xl-12">
+	<h2>Ajouter un commentaire</h2>
+</div>
 
-<form action="" method="post">
-	<p>
-		<?= isset($erreurs) && in_array(\Entity\Comment::AUTEUR_INVALIDE, $erreurs) ? 'L\'auteur est invalide <br />' : '' ?>
-		<label>Pseudo</label>
-		<input type="text" name="pseudo" value="<?= isset($comment) ? htmlspecialchars($comment['author']) : '' ?>" /><br />
+<div class="form col-xl-12">
+	<form action="" method="post">
+		<div class="form-group">
+			<?= isset($erreurs) && in_array(\Entity\Comment::AUTEUR_INVALIDE, $erreurs) ? 'L\'auteur est invalide <br />' : '' ?>
+			<label>Pseudo</label>
+			<input type="text" name="pseudo" value="<?= isset($comment) ? htmlspecialchars($comment['author']) : '' ?>" class="form-control" placeholder="Entrez votre pseudo"/><br />
 
-		<?= isset($erreurs) && in_array(\Entity\Comment::CONTENU_INVALIDE, $erreurs) ? 'Le commentaire n\'est pas valide <br />' : '' ?>
-		<label>Message</label>
-		<textarea name="contenu" rows="7" cols="50"><?= isset($comment) ? htmlspecialchars($comment$comment['content']) : '' ?></textarea>
+			<?= isset($erreurs) && in_array(\Entity\Comment::CONTENU_INVALIDE, $erreurs) ? 'Le commentaire n\'est pas valide <br />' : '' ?>
+			<label>Message</label>
+			<textarea name="content" class="form-control" rows="7" cols="50" placeholder="Votre message"><?= isset($comment) ? htmlspecialchars($comment['content']) : '' ?></textarea>
+		</div>
 
-		<input type="submit" value="Ajouter" />
+			<input type="submit" class="btn btn-primary" value="Ajouter" />
 
-	</p>
-
-</form>
+	</form>
+</div>
