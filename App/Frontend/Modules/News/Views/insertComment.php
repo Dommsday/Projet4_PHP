3ca6088e3d7d8a914c12b116992efa4071a5a4ac
Dommsday <Julienzimmermann@msn.com>
2018-10-16 11:16:13 +0200

    Modification de quelques erreurs de frappes

--- App/Frontend/Modules/News/Views/insertComment.php
+++ App/Frontend/Modules/News/Views/insertComment.php
@@ -4,9 +4,9 @@
 	<p>
 		<?= isset($erreurs) && in_array(\Entity\Comment::AUTEUR_INVALIDE, $erreurs) ? 'L\'auteur est invalide <br />' : '' ?>
 		<label>Pseudo</label>
-		<input type="text" name="pseudo" value="<?= isset($comment) ? htmlspecialchars($comment['author']) : '' ?>" /></br>
+		<input type="text" name="pseudo" value="<?= isset($comment) ? htmlspecialchars($comment['author']) : '' ?>" /><br />
 
-		<?= isset($erreurs) $$ in_array(\Entity\Comment::CONTENU_INVALIDE, $erreurs) ? 'Le commentaire n\'est pas valide <br />' : '' ?>
+		<?= isset($erreurs) && in_array(\Entity\Comment::CONTENU_INVALIDE, $erreurs) ? 'Le commentaire n\'est pas valide <br />' : '' ?>
 		<label>Message</label>
 		<textarea name="contenu" rows="7" cols="50"><?= isset($comment) ? htmlspecialchars($comment$comment['content']) : '' ?></textarea>
 
