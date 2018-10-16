0bb41b8200a522f6ccfe4932d25186cfb104d4b4
Dommsday <Julienzimmermann@msn.com>
2018-10-16 16:53:40 +0200

    Ecriture de la méthode ExecuteIndex

---
+++ App/Backend/Module/Connexion/ConnexionController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Backend\Module\Connexion;
+
+use \framework\BackController;
+use \framework\HTTPRequest;
+
+class ConnexionController extends BackController{
+
+	public function executeIndex(HTTPRequest $request){
+
+		$this->page->addVarPage('title', 'Connexion');
+
+		//Si le pseudo est rentré dans le formulaire
+		if($request->postExists('login')){
+
+			$login = $request->postData('login');
+			$password = $reques->postData('password');
+
+			//Si les valeurs entrées sont identiques au fichiers de configuration 
+			if($login == $this->app->config()->get('login') && $password == $this->app->config()->get->('pwd')){
+
+				$this->app->httpResponse->redirect('.');
+
+			}
+		}
+	}
+}
