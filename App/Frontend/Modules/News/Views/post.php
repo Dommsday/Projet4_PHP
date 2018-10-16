ef610b4dc7acde48a19c61f15e96044971e84512
Dommsday <Julienzimmermann@msn.com>
2018-10-16 15:35:10 +0200

    Ajout du style css

--- App/Frontend/Modules/News/Views/post.php
+++ App/Frontend/Modules/News/Views/post.php
@@ -1,29 +1,29 @@
-<p>Par <em><?= $post['author'] ?></em>, le <?= $post['date']->format('d/m/Y à H\hi') ?></p>
-<h2><?= $post['title'] ?></h2>
+<div class="info-comment col-xl-12">
+	<p>Par <em><?= $post['author'] ?></em>, le <?= $post['date']->format('d/m/Y à H\hi') ?></p>
+</div>
+
+<div class="title col-xl-12">
+	<h2><?= $post['title'] ?></h2>
+</div>
+
 <p><?= nl2br($post['content']) ?></p>
 
 
-<?php if($news['date'] != $news['update_date']){ ?>
-
-	<p>Modifié le <?= $news['update_date']->format('d/m/Y à H\hi') ?></p>
-<?php 
-}
-?>
-
-<p><a href="comment-news-<?= $news['id'] ?>.html">Ajouter un commentaire</a></p>
+<div class="container-comment">
+<p><a href="comment-news-<?= $post['id'] ?>.html">Ajouter un commentaires</a></p>
 
 <?php
-	if(empty($comment)){
+	if(empty($comments)){
 ?>
 
-<p>Aucun commentaire. Tu n'as pas envie d'$etre le premier??</p>
+<p>Aucun commentaire. Tu n'as pas envie d'etre le premier??</p>
 <?php
 }
 
 foreach ($comments as $comment){
 ?>
 
-<p>Posté par <?= htmlspecialchars($comment['author']) ?> le <?= $comment['date']->format('d/m/Y à H\hi') ?></p>
+<p class="info-author">Posté par <?= htmlspecialchars($comment['author']) ?> le <?= $comment['date']->format('d/m/Y à H\hi') ?></p>
 
 <p><?= $comment['content'] ?></p>
 
@@ -31,4 +31,5 @@ foreach ($comments as $comment){
 }
 ?>
 
-<p><a href="comment-news-<?= $news['id'] ?>.html">Ajouter un commentaire</a></p>
+<p><a href="comment-news-<?= $post['id'] ?>.html">Ajouter un commentaire</a></p>
+</div>
