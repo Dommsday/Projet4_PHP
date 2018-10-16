53780219bb5abef4c61b48521732eccebf9c5002
Dommsday <Julienzimmermann@msn.com>
2018-10-16 10:54:31 +0200

    Modification  dans la méthode add PDDO->PDO

--- library/vendors/Model/CommentManagerPDO.php
+++ library/vendors/Model/CommentManagerPDO.php
@@ -9,7 +9,7 @@ class CommentManagerPDO extends CommentManager{
 
 		$request = $this->dao->prepare('INSERT INTO comments SET news = :news, author = :author, content = :content, date = NOW() ');
 
-		$request->bindValue(':news', $comment->news(), \PDDO::PARAM_INT);
+		$request->bindValue(':news', $comment->news(), \PDO::PARAM_INT);
 		$request->bindValue(':author', $comment->author());
 		$request->bindValue(':content', $comment->content());
 
