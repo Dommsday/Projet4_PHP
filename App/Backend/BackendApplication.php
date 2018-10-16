e9efe9b78a5ce2edaee8a1c189a72718ec382297
Dommsday <Julienzimmermann@msn.com>
2018-10-16 15:58:35 +0200

    Création et écriture du fichier BackendApplication.php

---
+++ App/Backend/BackendApplication.php
@@ -0,0 +1,23 @@
+<?php
+namespace Backend;
+
+use \framework\Application;
+
+class BackendApplication extends Application{
+
+	public function __construct(){
+
+		parrent::__construct();
+
+		$this->name = 'Backend';
+	}
+
+	public function run(){
+
+		$controller = new Modules\Connexion\ConnexionController($this, 'Connexion', 'index');
+		$controler = execute();
+
+		$this->httpResponse->setPage($controller->page());
+		$this->httpResponse->send();
+	}
+}
