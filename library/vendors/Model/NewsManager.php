30dd9ce60a0022ee15977f748d7405e3ff63963c
Dommsday <Julienzimmermann@msn.com>
2018-10-17 15:40:29 +0200

    Ajout de la méthode 'count'

--- library/vendors/Model/NewsManager.php
+++ library/vendors/Model/NewsManager.php
@@ -9,4 +9,6 @@ abstract class NewsManager extends Manager{
 	abstract public function getList($debut = -1, $limite = -1);
     
     abstract public function getPost($id);
+
+    abstract public function count();
 }
