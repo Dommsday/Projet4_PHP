8bbcbf413d1d1e090f8d5806e2cb85d242758334
Dommsday <Julienzimmermann@msn.com>
2018-10-17 16:46:23 +0200

    Ajout d'une '/' dans le lien admin

--- App/Frontend/FrontendViews/layout.php
+++ App/Frontend/FrontendViews/layout.php
@@ -17,7 +17,7 @@
 					<a class="nav-link" href="/">Accueil</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="/admin">Admin</a>
+					<a class="nav-link" href="/admin/">Admin</a>
 				</li>
 			</ul>
 		</nav>
