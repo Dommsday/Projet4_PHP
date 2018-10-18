6333ddf1b7e0613c8e8158291edcd534aee9bbdf
Dommsday <Julienzimmermann@msn.com>
2018-10-18 17:22:18 +0200

    Ajout de la méthode 'add' et de la méthode 'save'

--- library/vendors/Model/NewsManager.php
+++ library/vendors/Model/NewsManager.php
@@ -11,4 +11,18 @@ abstract class NewsManager extends Manager{
     abstract public function getPost($id);
 
     abstract public function count();
+    
+    abstract protected function add(News $post);
+    
+     //Méthode qui s'écrit directement car elle n'est pas dépendante de la DAO
+    public function save(News $post){
+
+    	if($post->Valid()){
+
+    		$post->idNew() ? $this->add($post) : $this->modify($post);
+    	}else{
+
+    		throw new \RuntimeException('L\'article doit être validé pour être enregistré';
+    	}
+    }
 }
