8f0424648d2cdafba08bb4a7b2e9002a3f2a4f60
Dommsday <Julienzimmermann@msn.com>
2018-10-18 13:37:09 +0200

    Ajout de la classe User dans la méthode 'executeIndex'

--- App/Backend/Modules/Connexion/ConnexionController.php
+++ App/Backend/Modules/Connexion/ConnexionController.php
@@ -19,9 +19,13 @@ class ConnexionController extends BackController{
 			//Si les valeurs entrées sont identiques au fichiers de configuration 
 			if($login == $this->app->config()->get('login') && $password == $this->app->config()->get('pwd')){
 
+                $this->app->user()->setAuthenticated(true);
 				$this->app->httpResponse()->redirect('.');
 
-			}
+			}else{
+                
+                $this->app->user->setMessage('Le pseudo ou le mot de passe est incorrect');
+            }
 		}
 	}
 }
