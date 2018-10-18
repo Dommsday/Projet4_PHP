57c0ec059c47ea6bff825077f62d471c7061aa80
Dommsday <Julienzimmermann@msn.com>
2018-10-18 17:11:55 +0200

    Ajout des méthode 'executeInsert' et 'processForm'

--- App/Backend/Modules/News/NewsController.php
+++ App/Backend/Modules/News/NewsController.php
@@ -16,4 +16,40 @@ class NewsController extends BackController{
 		$this->page->addVarPage('listNews', $manager->getList());
 		$this->page->addVarPage('nombreNews', $manager->count());
 	}
+    
+    public function executeInsert(HTTPRequest $request){
+
+		if($request->postExists('author')){
+
+			$this->processForm($request);
+		}
+
+		$this->page->addVarPage('title', 'Ajour d\'un post');
+	}
+
+	public function processForm(HTTPRequest $request){
+
+		$post = new News([
+			'author' => $request->postData('author'),
+			'title' => $request->postData('title'),
+			'content' => $request->postData('content')
+		]);
+
+		if($request->postExists('id')){
+
+			$post->setId($request->postData('id'));
+		}
+
+		if($post->Valid()){
+			$this->managers->getManagerOf('News')->save($post);
+
+			$this->app->user()->setMessage($post->idNew()) ? 'L\'article à bien été ajouté !' : 'L\'article à bien été modifié !';
+
+		}else{
+
+			$this->page->addVarPage('erreurs', $post->erreurs());
+		}
+
+		$this->page->addVarPage('news', $post);
+	}
 }
