57565153c414e44e6ab71fd08aa79de629ef29c8
Dommsday <Julienzimmermann@msn.com>
2018-10-20 13:51:50 +0200

    Modification de la vie

--- App/Backend/Modules/News/Views/index.php
+++ App/Backend/Modules/News/Views/index.php
@@ -13,7 +13,7 @@
 
 	foreach ($listNews as $news) {
 
-		echo '<tr><td>'.$news['author'].'</td><td>'.$news['title'].'</td><td>'.$news['date']->format('d/m/Y à H\hi').'</td><td>'.($news['date'] == $news('update_date') ? ' - ' : 'le '.$news['update_date']->format('d/m/Y à H\di')), '</td><td><a href="news-update-'.$news['id'].'.html">U</a> <a href="news-delete-'.$news['id'].'.html">X</a></td></tr>';
+		echo '<tr><td>'.$news['author'].'</td><td>'.$news['title'].'</td><td> <a href="news-update-'.$news['id'].'.html">U</a> <a href="news-delete-'.$news['id'].'.html">X</a></td></tr>';
 	}
 	?>
 </table>
