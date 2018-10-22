426caced1d5346dafb32162b5ac77f65e59e8d83
Dommsday <Julienzimmermann@msn.com>
2018-10-22 16:48:46 +0200

    Création et écriture de la class 'StringField'

---
+++ library/framework/StringField.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace framework;
+
+class StringField extends Field{
+
+	protected $maxLength;
+
+	public function buildWidget(){
+
+		$widget = '';
+
+		if(!empty($this->errorMessage)){
+
+			$widget .= $this->errorMessage.'<br />';
+		}
+
+		$widget .= '<label>'.$this->label.'</label><input type="text" name="'.$this->name().'"';
+
+		if(!empty($this->value)){
+
+			$widget .= ' value="'.htmlspecialchars($this->value).'"';
+		}
+
+		if(!empty($this->maxLength)){
+
+			$widget = ' maxlenght="'.$this->maxLenght.'"';
+		}
+
+		return $widget .= ' />';
+	}
+
+	public function setMaxLength($maxLength){
+
+		$maxLength = (int) $maxLength;
+
+		if($maxlenght > 0){
+			$this->maxlenght = $maxlenght;
+
+		}else{
+
+			throw new \RuntimeException('La longueur maximal doit être un nombre supérieur à 0');
+		}
+	}
+
+
+}
