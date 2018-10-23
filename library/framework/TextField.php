3ef3b7e006a8653eefcb22dce5ca1468101a6b9b
Dommsday <Julienzimmermann@msn.com>
2018-10-23 10:27:27 +0200

    Ecriture de la 2eme partie

--- library/framework/TextField.php
+++ library/framework/TextField.php
@@ -25,6 +25,36 @@ class TextField extends Field{
 
 		if(!empty($this->rows)){
 			
+			$widget .= ' rows="'.$this->rows.'"';
+		}
+
+		$widget .= '>';
+
+		if(!empty($this->value)){
+
+			$widget .= htmlspecialchars($this->value);
+		}
+
+		return $widget.'</textarea>';
+	}
+
+	public function setCols($cols){
+
+		$cols = (int) $cols;
+
+		if($cols > 0){
+
+			$this->cols = $cols;
+		}
+	}
+
+	public function setRows($rows){
+
+		$rows = (int) $rows;
+
+		if($rows > 0){
+
+			$this->rows = $rows;
 		}
 	}
 }
