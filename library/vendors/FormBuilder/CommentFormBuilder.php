633915f2a5c92107f579bb3bb2a926d41bf9637f
Dommsday <Julienzimmermann@msn.com>
2018-10-24 10:24:52 +0200

    Ecrétiure de la classe 'CommentFormBuilder'

---
+++ library/vendors/FormBuilder/CommentFormBuilder.php
@@ -0,0 +1,33 @@
+<?php
+namespace FormBuilder;
+
+use \framework\FormBuilder;
+use \framework\StringField;
+use \framework\TextField;
+use \framework\MaxLengthValidator;
+use \framework\NotNullValidator;
+
+class CommentFormBuilder extends FormBuilder
+{
+  public function build()
+  {
+    $this->form->add(new StringField([
+        'label' => 'pseudo',
+        'name' => 'pseudo',
+        'maxLength' => 50,
+        'validators' => [
+          new MaxLengthValidator('L\'auteur spécifié est trop long', 50),
+          new NotNullValidator('Merci de spécifier l\'auteur du commentaire'),
+        ],
+       ]))
+       ->add(new TextField([
+        'label' => 'Content',
+        'name' => 'content',
+        'rows' => 7,
+        'cols' => 50,
+        'validators' => [
+          new NotNullValidator('Merci de spécifier votre commentaire'),
+        ],
+       ]));
+  }
+}
