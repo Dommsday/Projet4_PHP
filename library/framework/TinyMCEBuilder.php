95ba90a867d0fe19133dcb2406527ce2da99553e
Dommsday <Julienzimmermann@msn.com>
2018-10-27 17:15:53 +0200

    Création et écriture de la classe 'TinyMCEBuilder'

---
+++ library/framework/TinyMCEBuilder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace framework;
+
+abstract class TinyMCEBuilder{
+
+	protected $tinymce;
+
+	public function __construct(Entity $entity){
+
+		$this->setTinymce(new Form($entity));
+	}
+
+	abstract public function build();
+
+	public function setTinymce(Form $tinymce){
+
+		$this->tinymce = $tinymce;
+	}
+
+	public function tinymce(){
+		return $this->tinymce;
+	}
+}
