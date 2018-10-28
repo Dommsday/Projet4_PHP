bc231c31712a0f3aa05602215f6fc1cabe2b7f61
Dommsday <Julienzimmermann@msn.com>
2018-10-28 16:34:05 +0100

    Modification de la structure

--- App/Backend/Modules/News/Views/index.php
+++ App/Backend/Modules/News/Views/index.php
@@ -1,19 +1,33 @@
-<p>Il y a actuellement <?= $nombreNews ?> articles <p>
+<div class="title-admin col-lg-12">
+	<h1>Tableau de bord</h1>
+</div>
 
+<div class="informations col-lg-12">
+	<div class="news">
+			<h2>Articles</h2>
+			<p class="number"> <?= $nombreNews ?> </p>
+	</div>
+
+	<div class="comments">
+		<h2>Commentaires</h2>
+		<p class="number"><?= $nombreComments ?> </p>
+	</div>
+</div>
+
+<div class="all-resume col-lg-12">
 <table>
 	<tr>
-		<th>Auteur</th>
-		<th>Titre</th>
-		<th>Date</th>
-		<th>Modification</th>
-		<th>Action</th>
+		<th class="author">Auteur</th>
+		<th class="title">Titre</th>
+		<th class="date">Date</th>
+		<th class="action">Action</th>
 	</tr>
 
 	<?php
-
 	foreach ($listNews as $news) {
 
-		echo '<tr><td>'.$news['author'].'</td><td>'.$news['title'].'</td><td> <a href="news-update-'.$news['id'].'.html">U</a> <a href="news-delete-'.$news['id'].'.html">X</a></td></tr>';
+		echo '<tr><td>'.$news['author'].'</td><td>'.$news['title'].'</td><td>'.$news['date'].'</td><td> <a href="news-update-'.$news['id'].'.html"><i class="fas fa-pen"></i></a> <a href="news-delete-'.$news['id'].'.html"><i class="fas fa-trash-alt"></i></a></td></tr>';
 	}
 	?>
 </table>
+</div>
