ce0b5d52f46c8282e6241d74596d7b0c315b98ec
Dommsday <Julienzimmermann@msn.com>
2018-10-31 10:43:37 +0100

    Modification de la vue 'index'

--- App/Backend/Modules/News/Views/index.php
+++ App/Backend/Modules/News/Views/index.php
@@ -4,32 +4,14 @@
 
 <div class="informations col-lg-12">
 	<div class="news">
-			<h2>Articles</h2>
+			<a href="/admin/all-post.html"><h2>Articles</h2></a>
 			<p class="number"> <?= $nombreNews ?> </p>
 	</div>
 
 	<div class="comments">
-		<h2>Commentaires</h2>
+		<a href="/admin/all-comments.html"><h2>Commentaires</h2></a>
 		<p class="number"><?= $nombreComments ?> </p>
 	</div>
-</div>
-
-<div class="all-resume col-lg-12">
-<table>
-	<tr>
-		<th class="author">Auteur</th>
-		<th class="title">Titre</th>
-		<th class="date">Date</th>
-		<th class="action">Action</th>
-	</tr>
-
-	<?php
-	foreach ($listNews as $news) {
-
-		echo '<tr><td>'.$news['author'].'</td><td>'.$news['title'].'</td><td>'.$news['date'].'</td><td> <a href="news-update-'.$news['id'].'.html"><i class="fas fa-pen"></i></a> <a href="news-delete-'.$news['id'].'.html"><i class="fas fa-trash-alt"></i></a></td></tr>';
-	}
-	?>
-</table>
 </div>
 
 
