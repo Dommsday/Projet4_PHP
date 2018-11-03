892f6c76e534e5ed2433f51515e9d97884a53379
Dommsday <Julienzimmermann@msn.com>
2018-11-03 14:01:05 +0100

    Modification de la vue

--- App/Backend/Modules/Connexion/Views/index.php
+++ App/Backend/Modules/Connexion/Views/index.php
@@ -1,15 +1,15 @@
 <div class="title col-xl-12">
-	<h2>Connexion</h2>
+	<h2 class="title-form">Connexion</h2>
 </div>
 
-<div class="form col-xl-12">
-	<form action="" method="post">
+<div class="form-connexion col-xl-12">
+	<form class="form" action="" method="post">
 		<div class="form-group">
-			<label>Pseudo</label>
-			<input type="text" name="login"/><br />
+			<label for="login">Login</label>
+			<input type="text" id="login" name="login" class="form-control" placeholder="Entrez le login" /><br />
 
-			<label>Mot de Passe</label>
-			<input type="password" name="password" /><br />
+			<label for="mdp">Mot de Passe</label>
+			<input type="password" id="mdp " name="password" class="form-control" placeholder="Entrez le mot de passe"/><br />
 		</div>
 
 			<input type="submit" class="btn btn-primary" value="Connexion" />
