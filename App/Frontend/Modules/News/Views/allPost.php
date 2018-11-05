85ee7b6b13034dda100b5997ae6cbd86d8e62288
Dommsday <Julienzimmermann@msn.com>
2018-11-05 23:14:02 +0100

    Modification de la vue 'allPost'

--- App/Frontend/Modules/News/Views/allPost.php
+++ App/Frontend/Modules/News/Views/allPost.php
@@ -1,13 +1,23 @@
-<?php
-	foreach ($listNews as $news) {
-	?>
+<div class="container container-allpost">
 
-	<tr>
-		<td><a href="../news-<?= $news['id'] ?>.html"><?= $news['title'] ?></a></td>
-		<td><?= $news['date'] ?></td>
-		<td><?= $news['content'] ?></td>
-	</tr>
+		<?php
+			foreach ($listNews as $news) {
+		?>
 
-	<?php
-	}
-	?>
+	<div class="all-post-text col-lg-12">
+
+		<div class="title-all-post col-xl-12">
+			<h2><a href="../news-<?= $news['id'] ?>.html"><?= $news['title'] ?></a></h2>
+		</div>
+
+		<div class="info-comment col-xl-12">
+			<?= $news['date'] ?>
+		</div>
+
+		<?= $news['content'] ?>
+
+	</div>
+		<?php
+		}
+		?>
+</div>
