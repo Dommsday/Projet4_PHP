ca025c08f53f3ef1f0de72263517c976b94338e6
Dommsday <Julienzimmermann@msn.com>
2018-11-05 11:30:47 +0100

    Modification du widget 'label'

--- library/framework/StringField.php
+++ library/framework/StringField.php
@@ -14,7 +14,7 @@ class StringField extends Field
       $widget .= $this->errorMessage.'<br />';
     }
     
-    $widget .= '<label>'.$this->label.'</label><input type="text" name="'.$this->name.'"';
+    $widget .= '<label>'.$this->label.'</label><input type="text" name="'.$this->name.'" id="'.$this->id.'" class="'.$this->boots.'"';
     
     if (!empty($this->value))
     {
