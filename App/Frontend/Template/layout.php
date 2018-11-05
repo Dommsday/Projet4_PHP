73bd9162f166c6bf84b14ea1ec496a50606fc97e
Dommsday <Julienzimmermann@msn.com>
2018-11-05 19:09:51 +0100

    Ajout d'un lien 'Articles'

--- App/Frontend/Template/layout.php
+++ App/Frontend/Template/layout.php
@@ -25,6 +25,10 @@
 					<li class="nav-item">
 							<a class="nav-link" href="/">Accueil</a>
 					</li>
+					
+					<li class="nav-item">
+							<a class="nav-link" href="/all-post.html">Articles</a>
+					</li>
 
 					<li class="nav-item">
 						<a class="nav-link" href="/admin/">Admin</a>
