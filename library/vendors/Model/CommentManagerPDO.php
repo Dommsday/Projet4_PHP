fff76da483f1fb18e3369b3cd175fb80c957fdf2
Dommsday <Julienzimmermann@msn.com>
2018-11-05 16:12:28 +0100

    Ajout de l'instruction 'ORDER BY' dans la méthode 'getAllComment'

--- library/vendors/Model/CommentManagerPDO.php
+++ library/vendors/Model/CommentManagerPDO.php
@@ -43,7 +43,7 @@ class CommentManagerPDO extends CommentManager{
 
     public function getAllComment(){
 
-		$request = $this->dao->query('SELECT c.id AS id, c.author AS author, c.content AS comments, c.date AS date, c.warning AS warning, n.title AS title FROM comments AS c INNER JOIN news AS n ON c.news = n.id');
+		$request = $this->dao->query('SELECT c.id AS id, c.author AS author, c.content AS comments, c.date AS date, c.warning AS warning, n.title AS title FROM comments AS c INNER JOIN news AS n ON c.news = n.id ORDER BY title');
 
 		$comments = $request->fetchAll();	
 
